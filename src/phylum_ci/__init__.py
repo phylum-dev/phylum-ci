b6ed87733d0e9f94ab4b877fbdeffea3942f0532
Charles Coggins <charles@phylum.io>
2022-03-29 12:34:20 -0500

    Format strings and comment in top-level package

--- src/phylum_ci/__init__.py
+++ src/phylum_ci/__init__.py
@@ -1,8 +1,7 @@
 """Top-level package for phylum-ci."""
 
-__author__ = """Phylum, Inc."""
+__author__ = "Phylum, Inc."
 __email__ = "engineering@phylum.io"
-# TODO: bump this version to at least 0.1.0 once there is *any* amount of
-#       functionality provided by this package. Right now, this is just a
-#       placeholder to be able to "reserve" the package name on PyPI.
+# TODO: bump this version to at least 0.1.0 once there is more product
+#       centered functionality provided by this package.
 __version__ = "0.0.1"
