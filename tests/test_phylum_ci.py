1f93ec46c401edcbae029fe0572e567aa62f7f35
Charles Coggins <maxrake@users.noreply.github.com>
2022-04-04 15:51:54 -0500

    Add test for author and email metadata

--- tests/test_phylum_ci.py
+++ tests/test_phylum_ci.py
@@ -2,19 +2,19 @@ import pathlib
 import sys
 
 import tomli
-from phylum_ci import __version__
+from phylum_ci import __author__, __email__, __version__
 
 HERE = pathlib.Path(__file__).resolve().parent
 PROJECT_ROOT = HERE.parent
 PYPROJECT_TOML_PATH = PROJECT_ROOT / "pyproject.toml"
 
+with open(PYPROJECT_TOML_PATH, "rb") as f:
+    PYPROJECT = tomli.load(f)
 
-def test_version():
+
+def test_project_version():
     """Ensure the package version string matches the reported project version."""
-    with open(PYPROJECT_TOML_PATH, "rb") as f:
-        pyproject = tomli.load(f)
-    pyproject_version = pyproject.get("tool", {}).get("poetry", {}).get("version")
-    assert __version__ == pyproject_version
+    assert __version__ == PYPROJECT.get("tool", {}).get("poetry", {}).get("version")
 
 
 def test_python_version():
@@ -23,3 +23,14 @@ def test_python_version():
     python_version = sys.version_info
     assert python_version.major == 3, "Only Python 3 is supported"
     assert python_version.minor in supported_minor_versions
+
+
+def test_author_email_metadata():
+    """Ensure the project and package metadata for author and email match and are correct."""
+    assert __author__ == "Phylum, Inc.", "The company name should be used instead of individual developers"
+    assert __email__ == "engineering@phylum.io", "The engineering group email account should be used"
+    # Package authors in Poetry are specified as a list of "name <email>" entries
+    expected_poetry_author = f"{__author__} <{__email__}>"
+    poetry_authors = PYPROJECT.get("tool", {}).get("poetry", {}).get("authors", [])
+    assert expected_poetry_author in poetry_authors
+    assert len(poetry_authors) == 1, "There should only be one author - the company, with it's engineering group email"
