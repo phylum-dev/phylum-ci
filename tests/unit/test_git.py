06bd040874cbe4fc86594163c0959d9be3dd0ca7
Charles Coggins <maxrake@users.noreply.github.com>
2023-03-27 16:30:32 -0500

    test: refactor tests to avoid skip logic

--- tests/unit/test_git.py
+++ tests/unit/test_git.py
@@ -7,16 +7,19 @@ from dulwich import porcelain
 
 from phylum.ci.git import git_repo_name
 
-# Names of a git repository that will be initialized locally
-LOCAL_REPO_NAMES = [
+# Names of a git repository that will be cloned locally
+CLONED_LOCAL_REPO_NAMES = [
     "local_repo_name",
     "local_repo.name",
-    "local_repo.git",
     "local.repo.name",
 ]
 
+# Names of a git repository that will be initialized locally
+# Separate lists are used because cloned local repos with a name ending in `.git` are not supported
+INITIALIZED_LOCAL_REPO_NAMES = CLONED_LOCAL_REPO_NAMES + ["local_repo.git"]
 
-@pytest.mark.parametrize("repo_name", LOCAL_REPO_NAMES)
+
+@pytest.mark.parametrize("repo_name", INITIALIZED_LOCAL_REPO_NAMES)
 def test_initialized_local_repo_name(tmp_path, repo_name):
     """Ensure a local repo, with no remotes, has it's name correctly identified."""
     repo_path: Path = tmp_path / repo_name
@@ -25,11 +28,9 @@ def test_initialized_local_repo_name(tmp_path, repo_name):
     assert local_repo_name == repo_name, "The local repo name should be found"
 
 
-@pytest.mark.parametrize("repo_name", LOCAL_REPO_NAMES)
+@pytest.mark.parametrize("repo_name", CLONED_LOCAL_REPO_NAMES)
 def test_cloned_local_repo_name(tmp_path, repo_name: str):
     """Ensure a cloned local repo has it's name correctly identified."""
-    if repo_name.endswith(".git"):
-        pytest.skip("Cloned local repos with a name ending in `.git` are not supported")
     init_repo_path: Path = tmp_path / repo_name
     cloned_repo_path: Path = tmp_path / "cloned_local_repo_name"
     porcelain.init(str(init_repo_path))
