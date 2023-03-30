266f5c07abb9c688f5111918d4289a0db1bbfee5
Charles Coggins <maxrake@users.noreply.github.com>
2023-03-30 10:00:35 -0500

    refactor: use standard lib `importlib.metadata`
    This replaces the backported `importlib_metadata` package.

--- src/phylum/__init__.py
+++ src/phylum/__init__.py
@@ -1,15 +1,9 @@
 """Top-level package for phylum."""
-# TODO: Use only the standard library form (importlib.metadata) after Python 3.7 support is dropped
-#       https://github.com/phylum-dev/phylum-ci/issues/18
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
+from importlib.metadata import metadata, version
 
+PKG_METADATA = metadata(__name__)
 
-PKG_METADATA = importlib_metadata.metadata(__name__)
-
-__version__ = importlib_metadata.version(__name__)
+__version__ = version(__name__)
 __author__ = PKG_METADATA["Author"]
 __email__ = PKG_METADATA["Author-email"]
 
