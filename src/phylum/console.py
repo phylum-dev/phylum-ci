e51c433755dd2822ac4052614d90099ee108ae77
Charles Coggins <maxrake@users.noreply.github.com>
2023-04-14 12:09:25 -0500

    fix: link to Phylum UI project clipped in logs
    With the change to policy based operation, the analysis report is no
    longer generated within `phylum-ci`. Instead it comes from the `report`
    element of the "evaluate policy" endpoint offered by the API. This
    report includes a link to view the project in the Phylum UI. The link is
    often longer than the width of the terminal used in some CI environments
    which causes the link to get "clipped" and therefore no longer
    functional as a clickable URL. This is a regression of issue #140, which
    was previously fixed in #186.
    
    This change removes the `connect-markdown-renderer` dependency and adds
    `rich` as a main dependency. `connect-markdown-renderer` was chosen
    before because it extends the markdown rendering functionality provided
    by `rich` to include additional elements like tables. The new report
    format does not include tables and the markdown it does have is all
    renderable by `rich`, which _does_ allow for more control over the
    console.
    
    A `console` module was added to provide a console object that can be
    used throughout the application. That console makes use of a custom
    Phylum theme that sets unique styles for a subset of the markdown styles
    that `rich` supports. The console is used to print the analysis report
    as rendered markdown, but without using hyperlinks. Most importantly,
    soft wrap is enabled for any text that doesn't fit so it will run on to
    the following line(s) instead of broken up by having line breaks
    inserted.
    
    Ref: #140

---
+++ src/phylum/console.py
@@ -0,0 +1,46 @@
+"""Provide a console object that can be used throughout the application.
+
+The `rich` library is used here for excellent control over the console.
+Reference: https://rich.readthedocs.io/en/latest/index.html
+"""
+
+from rich.console import Console
+from rich.theme import Theme
+
+# Create a custom Phylum theme by setting unique style values for a subset of the markdown styles that `rich` supports.
+# The full list of markdown styles, as of the time of this writing, is included here to make it easier to understand
+# what can be modified. The default styles are also included so any future changes will not cause unexpected output.
+# The colors chosen are from the standard 8-bit colors supported in terminals and are close to the range of blue colors
+# that make up the Phylum logo. These colors are also able to be represented by a name instead of a hex or RGB value.
+# References:
+#   * https://rich.readthedocs.io/en/latest/style.html
+#   * https://rich.readthedocs.io/en/latest/appendix/colors.html
+PHYLUM_STYLES = {
+    # Styles that have been modified from the default:
+    "markdown.code": "bold reverse",
+    "markdown.h1.border": "dodger_blue2",
+    "markdown.h1": "dodger_blue2",
+    "markdown.h2": "deep_sky_blue3",
+    "markdown.h3": "underline cornflower_blue",
+    "markdown.h4": "steel_blue1",
+    "markdown.h5": "turquoise2",
+    "markdown.h6": "cyan1",
+    "markdown.h7": "bright_cyan",
+    "markdown.item.bullet": "bold blue",
+    "markdown.item.number": "bold blue",
+    # The `rich` default styles:
+    "markdown.strong": "bold",
+    "markdown.emph": "italic",
+    "markdown.paragraph": "none",
+    "markdown.text": "none",
+    "markdown.code_block": "dim cyan on black",
+    "markdown.block_quote": "magenta",
+    "markdown.list": "cyan",
+    "markdown.item": "none",
+    "markdown.hr": "yellow",
+    "markdown.link": "bright_blue",
+    "markdown.link_url": "blue",
+}
+
+phylum_theme = Theme(styles=PHYLUM_STYLES)
+console = Console(theme=phylum_theme)
