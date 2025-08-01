e9889fa9aee6615328b603137ffc76e1ff2b4e33
Charles Coggins <maxrake@users.noreply.github.com>
2025-08-01 15:45:47 -0500

    test: add more unit tests for the `git` module
    Add new tests to cover the following functions in the `git` module:
    
    * `git_remote`
    * `git_base_cmd`
    * `ensure_git_repo_access`
    * `git_fetch`
    * `git_set_remote_head`
    * `git_default_branch_name`
    * `git_root_dir`
    * `git_current_branch_name`
    * `git_hash_object`
    * `git_repo_name`
    * `git_worktree`
    
    Additional changes include:
    
    * Add `only_run_if_ci` fixture for slow tests
    * Account for unknown errors when ensuring git repo access
    * Format and refactor throughout
      * Import the `git` module instead of all git functions individually

--- tests/constants.py
+++ tests/constants.py
@@ -1,7 +1,9 @@
 """Place test package constants here."""
 
+import os
 import pathlib
 
+import pytest
 import tomli
 
 HERE = pathlib.Path(__file__).resolve().parent
@@ -10,3 +12,6 @@ PYPROJECT_TOML_PATH = PROJECT_ROOT / "pyproject.toml"
 
 with PYPROJECT_TOML_PATH.open("rb") as f:
     PYPROJECT = tomli.load(f)
+
+# This pytest fixture is used to mark tests that should only run in a CI environment (e.g., too slow for local testing)
+only_run_if_ci = pytest.mark.skipif(not os.getenv("CI"), reason="Test should only run in CI")
